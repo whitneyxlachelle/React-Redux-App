4dd81816fc58d10993a10a465f5c4b31737193c7
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-05 17:58:22 -0600

    createstore in place

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <h1>Helloooo!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
