730c97d094af32d1153777388120df508324b367
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-07 19:27:15 -0600

    basic styling

--- app/src/components/Response.js
+++ app/src/components/Response.js
@@ -6,15 +6,30 @@ import { getResponse } from "../store/actions";
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
     root: {
-        width: '50%',
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
+        maxWidth: '40%',
+        maxHeight: '100%',
+        margin: 'auto',
     },
-}));
+    button: {
+        margin: 'auto',
+        background: 'red',
+        color: 'black',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+      }
+});
 
 const Response = (props) => {
     console.log(props)
@@ -34,10 +49,26 @@ const Response = (props) => {
 
     return (
         <div>
-                 {props.answer && !props.isFetching && <h2>{props.answer.answer}</h2>}
-                 {props.image && !props.isFetching && <img src={props.image.image} alt="Your Answer" width="460" height="345"/>}
-                
-                <button onClick={props.getResponse}>Yes or No</button>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={props.image.image}
+                        title="Gif"
+                    />
+                    <CardContent>
+                        {props.answer && !props.isFetching &&
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {props.answer.answer}
+                            </Typography>}
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button className={classes.button} size="large" color="primary" onClick={props.getResponse}>
+                        Yes/No
+        </Button>
+                </CardActions>
+            </Card>
         </div>
     );
 };
